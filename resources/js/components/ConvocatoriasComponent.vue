1bdb4d57d842ffb1a9b9c23974bc7751a4aeb1d1
AlfredoHerreraG <h.alfredo1507@gmail.com>
2019-01-16 13:54:04 -0600

    agrega vista index de covocatorias

--- resources/js/components/ConvocatoriasComponent.vue
+++ resources/js/components/ConvocatoriasComponent.vue
@@ -1,14 +1,60 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
+    <div class="font-sans bg-grey-lighter flex flex-col min-h-screen">
+        <div class="container mx-auto px-4 flex flex-wrap">
+            <div class="p-1 w-full sm:w-full md:w-1/2">
+                <a href="#" class="no-underline">
+                    <div class="group relative rounded overflow-hidden shadow-lg hover:opacity-75">
+                        <i class="fas fa-edit pin-r text-black text-xl-6 absolute invisible group-hover:visible"></i>
+                        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2 text-grey-darkest">Título de la convocatoria</div>
+                            <p class="font-bold text-grey">Categoría</p> 
+                            <p class="my-3 text-grey-darker text-base">Descripción</p>
+                        </div>
+                        <div class="px-6 py-4">
+                            <span class="text-sm font-semibold text-grey-darker">Last updated 3 min ago</span>
+                        </div>
                     </div>
-                </div>
+                </a>
+            </div>
+            <div class="p-1 w-full sm:w-full md:w-1/2">
+                <a href="#" class="no-underline">
+                    <div class="group relative rounded overflow-hidden shadow-lg hover:opacity-75">
+                        <i class="fas fa-edit pin-r text-black text-xl-6 absolute invisible group-hover:visible"></i>
+                        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2 text-grey-darkest">Título de la convocatoria</div>
+                            <p class="font-bold text-grey">Categoría</p> 
+                            <p class="my-3 text-grey-darker text-base">Descripción</p>
+                        </div>
+                        <div class="px-6 py-4">
+                            <span class="text-sm font-semibold text-grey-darker">Last updated 3 min ago</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="p-1 w-full sm:w-full md:w-1/2">
+                <a href="#" class="no-underline">
+                    <div class="group relative rounded overflow-hidden shadow-lg hover:opacity-75">
+                        <i class="fas fa-edit pin-r text-black text-xl-6 absolute invisible group-hover:visible"></i>
+                        <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2 text-grey-darkest">Título de la convocatoria</div>
+                            <p class="font-bold text-grey">Categoría</p> 
+                            <p class="my-3 text-grey-darker text-base">Descripción</p>
+                        </div>
+                        <div class="px-6 py-4">
+                            <span class="text-sm font-semibold text-grey-darker">Last updated 3 min ago</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="p-1 w-full sm:w-full md:w-1/2">
+                <a href="#" class="no-underline">
+                    <div class="group relative rounded flex items-center justify-center p-5 overflow-hidden shadow-lg hover:opacity-75 h-full">
+                        <i class="fas fa-plus-circle text-grey-darker text-xl-10"></i>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
@@ -17,7 +63,7 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            //
         }
     }
 </script>
