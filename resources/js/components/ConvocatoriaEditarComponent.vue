054b0d03d24cd07848ed41a885415f917113a0c9
AlfredoHerreraG <h.alfredo1507@gmail.com>
2019-01-17 18:29:28 -0600

    agranda margen inferior de los títulos

--- resources/js/components/ConvocatoriaEditarComponent.vue
+++ resources/js/components/ConvocatoriaEditarComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-white border-t border-b sm:border-l sm:border-r sm:rounded shadow mb-6">
         <div class="border-b p-6">
-            <h1 class="mb-4 text-gob">Editar convocatoria</h1>
+            <h1 class="mb-5 text-gob">Editar convocatoria</h1>
             <ConvocatoriaFormComponent></ConvocatoriaFormComponent>
         </div>
     </div>
